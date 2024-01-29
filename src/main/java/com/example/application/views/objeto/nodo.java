eecd7b70783d683f3e18ad7423c69c948f3c0dd5
Seth Millan <millanseth@gmail.com>
2024-01-29 13:47:09 -0600

    add:Programacion del nodo

--- src/main/java/com/example/application/views/objeto/nodo.java
+++ src/main/java/com/example/application/views/objeto/nodo.java
@@ -1,4 +1,29 @@
 package com.example.application.views.objeto;
 
 public class nodo {
+    private String nombre, direccion;
+    private nodo siguiente, anterior;
+
+    public nodo(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        siguiente=null;
+        anterior=null;
+    }
+
+    public nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public nodo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(nodo anterior) {
+        this.anterior = anterior;
+    }
 }
