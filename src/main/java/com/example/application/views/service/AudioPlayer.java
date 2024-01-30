349ab29926cc927164a50b752e035c8bcdf66f62
Seth Millan <millanseth@gmail.com>
2024-01-29 22:03:14 -0600

    add: Implementacion de la interfaz del servicio

--- src/main/java/com/example/application/views/service/AudioPlayer.java
+++ src/main/java/com/example/application/views/service/AudioPlayer.java
@@ -1,5 +1,65 @@
 package com.example.application.views.service;
 
-public class AudioPlayer {
+import com.example.application.views.objeto.nodo;
 
+public class AudioPlayer implements IAudioPlayer{
+    private nodo inicioLista, finLista;
+    private int tamanio;
+    //-----Constructor-----------------------
+    public AudioPlayer(){
+        inicioLista=null;
+        finLista=null;
+        tamanio=0;
+    }
+    //------Metodos--------------------------
+    @Override
+    public boolean isEmpty() {
+        if (inicioLista==null&&finLista==null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void insertar(String nom, String dir) {
+        nodo N=new nodo(nom,dir);
+        if(inicioLista==null){
+            inicioLista=N;
+            inicioLista.setSiguiente(inicioLista);
+            N.setAnterior(finLista);
+            finLista=N;
+
+        }else{
+            finLista.setSiguiente(N);
+            N.setSiguiente(inicioLista);
+            N.setAnterior(finLista);
+            finLista=N;
+            inicioLista.setAnterior(finLista);
+        }
+        tamanio++;
+    }
+
+    @Override
+    public int getTamanio() {
+        if (inicioLista==null&&finLista==null){
+            return 0;
+        }else{
+            return tamanio;
+        }
+    }
+
+    @Override
+    public nodo getCancion(int index) {
+        if (index < 0 || index >= tamanio) {
+            return null;
+        }
+        int n = 0;
+        nodo aux = inicioLista;
+        while (n != index) {
+            aux = aux.getSiguiente();
+            n++;
+        }
+        return aux;
+    }
 }
