08e7a2c0be4c44cc9172657bff36103423c63069
Seth Millan <millanseth@gmail.com>
2024-01-29 22:02:44 -0600

    add: Interfaz del Servicio

--- src/main/java/com/example/application/views/service/IAudioPlayer.java
+++ src/main/java/com/example/application/views/service/IAudioPlayer.java
@@ -1,4 +1,11 @@
 package com.example.application.views.service;
 
+import com.example.application.views.objeto.nodo;
+
 public interface IAudioPlayer {
+    boolean isEmpty();
+    void insertar(String nom,String dir);
+    int getTamanio();
+    nodo getCancion(int index);
+
 }
