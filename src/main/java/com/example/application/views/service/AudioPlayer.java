90894c675561de2358d815faaf68d40bdb6249fd
Seth Millan <millanseth@gmail.com>
2024-02-04 00:19:33 -0600

    update: funcion de mostrar en el grid agregada

--- src/main/java/com/example/application/views/service/AudioPlayer.java
+++ src/main/java/com/example/application/views/service/AudioPlayer.java
@@ -1,6 +1,13 @@
 package com.example.application.views.service;
 
 import com.example.application.views.objeto.nodo;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AudioPlayer implements IAudioPlayer{
     private nodo inicioLista, finLista;
@@ -20,7 +27,23 @@ public class AudioPlayer implements IAudioPlayer{
             return false;
         }
     }
+    @Override
+    public void mostrarCanciones(Grid<nodo> grid){
 
+        List<nodo> canciones=new ArrayList<>();
+        try{
+            nodo nodoActual = inicioLista;
+
+            while (nodoActual != null) {
+                canciones.add(nodoActual);
+                nodoActual = nodoActual.getSiguiente();
+            }
+            grid.setItems(canciones);
+        }
+        catch(Exception e){
+            Notification.show("Errorcito: "+e);
+        }
+}
     @Override
     public void insertar(String nom, String dir) {
         nodo N=new nodo(nom,dir);
--- src/main/java/com/example/application/views/service/IAudioPlayer.java
+++ src/main/java/com/example/application/views/service/IAudioPlayer.java
@@ -1,11 +1,13 @@
 package com.example.application.views.service;
 
 import com.example.application.views.objeto.nodo;
+import com.vaadin.flow.component.grid.Grid;
 
 public interface IAudioPlayer {
     boolean isEmpty();
     void insertar(String nom,String dir);
     int getTamanio();
     nodo getCancion(int index);
+    void mostrarCanciones(Grid<nodo> grid);
 
 }
