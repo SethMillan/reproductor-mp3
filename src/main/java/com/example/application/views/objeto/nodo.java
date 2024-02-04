48f8cfbfe757f2d60ee02fae2bffeedffa59140f
Seth Millan <millanseth@gmail.com>
2024-02-04 00:21:09 -0600

    update: recorte de metodos por lombok

--- src/main/java/com/example/application/views/objeto/nodo.java
+++ src/main/java/com/example/application/views/objeto/nodo.java
@@ -1,5 +1,7 @@
 package com.example.application.views.objeto;
 
+import lombok.*;
+@Data
 public class nodo {
     private String nombre, direccion;
     private nodo siguiente, anterior;
@@ -7,23 +9,7 @@ public class nodo {
     public nodo(String nombre, String direccion) {
         this.nombre = nombre;
         this.direccion = direccion;
-        siguiente=null;
-        anterior=null;
-    }
-
-    public nodo getSiguiente() {
-        return siguiente;
-    }
-
-    public void setSiguiente(nodo siguiente) {
-        this.siguiente = siguiente;
-    }
-
-    public nodo getAnterior() {
-        return anterior;
-    }
-
-    public void setAnterior(nodo anterior) {
-        this.anterior = anterior;
+        siguiente = null;
+        anterior = null;
     }
 }
