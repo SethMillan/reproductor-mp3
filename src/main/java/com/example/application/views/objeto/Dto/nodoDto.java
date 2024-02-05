a1fb69dc05f8ecc86f70d6a15f51338a6dabdfc9
Seth Millan <millanseth@gmail.com>
2024-02-05 05:26:09 -0600

    update: metodos lombok agregados

--- src/main/java/com/example/application/views/objeto/Dto/nodoDto.java
+++ src/main/java/com/example/application/views/objeto/Dto/nodoDto.java
@@ -1,12 +1,16 @@
 package com.example.application.views.objeto.Dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
 
 @Data
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class nodoDto implements Serializable {
     private String nombre, direccion;
 }
